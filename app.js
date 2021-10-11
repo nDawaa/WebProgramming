19a8a57dba113feb8ac6f8ebede4814b8e5cc0d6
nDawaa <davaa.mgl@gmail.com>
2021-10-11 11:58:11 +0800

    post form handling

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
+
+const app = express()
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+ 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname +"/index.html")
+  })
+
+//  // Handling querystring 
+  // app.get('/Blog/post?id=11&page=1', function (req, res) {
+
+  //   console.log(req.query.id, req.query.id)
+  //   res.send('<h1> Please check your console </h1>');
+  // })
+
+  app.get('/Contact', function (req, res) {
+    res.sendFile(__dirname +"/contact.html")
+  })
+
+  app.post("/ContactForm", (req, res) => {
+
+    var _email  = req.body.txtEmail;
+    var _phone  = req.body.txtNumber;
+    var _subject  = req.body.txtSubject;
+    var _body  = req.body.txtBody;
+
+    console.log(_email, _phone, _subject, _body);
+
+    res.send("Contact information received successfully. Please check your console now.");
+    });
+
+  app.get('/contact.html', function (req, res) {
+    res.redirect('/Contact')
+  })
+
+  app.get('/index.html', function (req, res) {
+    res.redirect('/')
+  })
+
+  // app.use(express.static(__dirname + '/public'));
+  app.use('/CSS', express.static('CSS'));
+  app.use('/Images', express.static('Images'));
+   
+app.listen(3000, function() {
+    console.log("The server is listening at port 3000");
+})
